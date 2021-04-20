af303ce1d05a236b9f7b97076f8eddc31f7629ba
张聪聪 <1316435855@qq.com>
2021-04-20 16:25:54 +0800

    1.修改  创建 生产者和消费者 2.生产者可以连接linux上用docker部署的rabbitMQ发送消息 3.消费者可以连接rabbitMQ 消费消息

--- rabbitmq/src/main/java/com/zcc/rabbitmq/simple/rabbitMGconsumer.java
+++ rabbitmq/src/main/java/com/zcc/rabbitmq/simple/rabbitMGconsumer.java
@@ -1,12 +1,58 @@
 package com.zcc.rabbitmq.simple;
 
+import com.rabbitmq.client.*;
+import com.rabbitmq.client.impl.AMQImpl;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * @author zcc
  * @ClassName rabbitMGconsumer
- * @description
+ * @description 消费者
  * @date 2021/4/19 19:39
  * @Version 1.0
  */
 
 public class rabbitMGconsumer {
+
+    private static String host = "47.93.228.227";
+    private static String userName = "guest";
+    private static String passWord = "guest";
+    //用docker部署的rabbitMQ 内部的5672映射到linux服务器上的8084
+    // （为啥不映射到linux上的5672端口？狗日的，阿里云安全组配置的5672总是不生效！）
+    private static int port = 8085;
+
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(host);
+        factory.setPort(port);
+        factory.setUsername(userName);
+        factory.setPassword(passWord);
+
+        Channel channel = null;
+        Connection connect = null;
+        try {
+            connect = factory.newConnection("消费者");
+            channel = connect.createChannel();
+
+            String queueName = "queue5";
+            channel.queueDeclare(queueName,false,false,false,null);
+            QueueingConsumer consumer = new QueueingConsumer(channel);
+            channel.basicConsume(queueName, true, consumer);
+            while (true) {
+                QueueingConsumer.Delivery deliver = consumer.nextDelivery();
+                System.out.println("reciver messager:"+new String(deliver.getBody()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            //关闭连接
+            rabbitMQproducer.closeConnect(channel, connect);
+        }
+    }
 }
--- rabbitmq/src/main/java/com/zcc/rabbitmq/simple/rabbitMQproducer.java
+++ rabbitmq/src/main/java/com/zcc/rabbitmq/simple/rabbitMQproducer.java
@@ -19,7 +19,9 @@ public class rabbitMQproducer {
     private static String host = "47.93.228.227";
     private static String userName = "guest";
     private static String passWord = "guest";
-    private static int port = 5672;
+    //用docker部署的rabbitMQ 内部的5672映射到linux服务器上的8084
+    // （为啥不映射到linux上的5672端口？狗日的，阿里云安全组配置的5672总是不生效！）
+    private static int port = 8085;
 
     public static void main(String[] args) {
 
@@ -32,32 +34,41 @@ public class rabbitMQproducer {
             Channel channel = null;
             Connection connect = null;
         try {
+            //创建连接connect
             connect = factory.newConnection("生产者");
+            //通过连接获取通道Channel
             channel = connect.createChannel();
-            String queueName = "queue1";
+            String queueName = "queue5";
+            //通过创建交换机，声明队列，绑定关系，路由key，发送消息和接收消息
             channel.queueDeclare(queueName,false,false,false,null);
+            channel.basicPublish("",queueName,null, "hello RabbitMQ".getBytes("UTF-8"));
 
-            System.out.println("----发送消息成功-----");
+            System.out.println("----发送消息5成功-----");
         } catch (IOException e) {
             e.printStackTrace();
         } catch (TimeoutException e) {
             e.printStackTrace();
         } finally {
-            if (channel != null && channel.isOpen()) {
-                try {
-                    channel.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (TimeoutException e) {
-                    e.printStackTrace();
-                }
+            //关闭连接
+            closeConnect(channel, connect);
+        }
+    }
+    //将关闭连接的代码提出来，改成一个方法
+    static void closeConnect(Channel channel, Connection connect) {
+        if (channel != null && channel.isOpen()) {
+            try {
+                channel.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
             }
-            if (connect != null && connect.isOpen()) {
-                try {
-                    connect.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        }
+        if (connect != null && connect.isOpen()) {
+            try {
+                connect.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
